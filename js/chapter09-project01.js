830932c9a84005980b2378268fc7a2cbcbbaa204
vinayreddie <vinayreddhek@gmail.com>
2021-01-25 10:44:23 -0500

    Update chapter09-project01.js

--- js/chapter09-project01.js
+++ js/chapter09-project01.js
@@ -1,2 +1,18 @@
+window.onload() = function() {
 
-/* add code here  */
+    var hilightables = document.getElementsByClassName("hilightable");
+    for (var i = 0; i < hilightables.length; i++) {
+        (function(i) {
+            var element = hilightables[i];
+
+            element.addEventListener("focus", function() {
+                element.classList.toggle("highlight");
+            });
+
+            element.addEventListener("blur", function() {
+                element.classList.toggle("highlight");
+            });
+        })(i);
+    }
+
+}
